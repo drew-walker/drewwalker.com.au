6d7d263b2accd7c4dc1d6bb2a753bbbe66afcd92
Drew Walker <drew.walker@gmail.com>
2014-02-08 09:38:59 +1100

    Updated web.js to serve index.html

--- app/web.js
+++ app/web.js
@@ -8,7 +8,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-    res.send('Hello World!');
+    res.sendfile(__dirname + '/index.html');
 });
 
 var port = Number(process.env.PORT || 5000);
