1925b46e6a530d26da5b25e9367db4e434690204
Drew Walker <drew.walker@gmail.com>
2014-02-08 11:13:44 +1100

    Added static content serving

--- app/web.js
+++ app/web.js
@@ -11,6 +11,12 @@ app.get('/', function(req, res) {
     res.sendfile(__dirname + '/index.html');
 });
 
+app.use(express.static(__dirname + '/fonts'));
+app.use(express.static(__dirname + '/images'));
+app.use(express.static(__dirname + '/scripts'));
+app.use(express.static(__dirname + '/styles'));
+app.use(express.static(__dirname + '/views'));
+
 var port = Number(process.env.PORT || 5000);
 
 app.listen(port, function() {
